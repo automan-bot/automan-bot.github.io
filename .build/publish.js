3a407d9dda342f1e8a6c452c9cfa1002e9a1277b
automan <autojs@126.com>
2024-02-26 17:24:17 +0800

    fix

--- .build/publish.js
+++ .build/publish.js
@@ -5,6 +5,11 @@ async function example() {
   const client = new ftp.Client();
   client.ftp.verbose = true;
   try {
+    if (
+      !(process.env.FTP_HOST && process.env.FTP_USER && process.env.FTP_PWD)
+    ) {
+      throw new Error("未设置服务器信息");
+    }
     const mFtpOptions = {
       host: process.env.FTP_HOST,
       port: (process.env.FTP_PORT && Number(process.env.FTP_PORT)) || 21,
@@ -13,9 +18,6 @@ async function example() {
       secure: false,
     };
     console.table(mFtpOptions);
-    if (process.env.FTP_HOST && process.env.FTP_USER && process.env.FTP_PWD) {
-      throw new Error("未设置服务器信息");
-    }
     await client.access(mFtpOptions);
     await client.removeDir("/en");
     await client.removeDir("/media");
