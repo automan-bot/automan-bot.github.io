2f3b7f56312da6a778294a55113c4a2a82a6b723
automan <autojs@126.com>
2024-05-14 20:33:29 +0800

    fix

---
+++ .github/publish.mjs
@@ -0,0 +1,40 @@
+import path from "path";
+import * as ftp from "basic-ftp";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const resolve = (p) => path.resolve(__dirname, p);
+async function example() {
+  const client = new ftp.Client();
+  client.ftp.verbose = true;
+  if (!(process.env.FTP_HOST && process.env.FTP_USER && process.env.FTP_PWD)) {
+    throw new Error("FTP_HOST,FTP_USER,FTP_PWD is required");
+  }
+  try {
+    const mFtpOptions = {
+      host: process.env.FTP_HOST || "ftp.cz128.com",
+      port: (process.env.FTP_PORT && Number(process.env.FTP_PORT)) || 21,
+      user: process.env.FTP_USER || "yugege",
+      password: process.env.FTP_PWD || "6B9B566A84d17d",
+      secure: false,
+    };
+    console.table(mFtpOptions);
+    await client.access(mFtpOptions);
+    await client.cd("/yugege/web");
+    await client.removeDir("autobot");
+    await client.removeDir("autobot_doc");
+    await client.removeDir("autojs");
+    /*     await client.uploadFromDir(resolve("../autobot"));
+    await client.uploadFromDir(resolve("../autobot_doc"));
+    await client.uploadFromDir(resolve("../autojs")); */
+
+    console.log(await client.list());
+  } catch (err) {
+    console.log(err);
+  }
+  client.close();
+}
+
+example();
